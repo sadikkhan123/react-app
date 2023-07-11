2370b6fe4a6aa64904fd67dbfae9b6a8dfd70a95
sadikkhan123 <sadikkhalidkhan@gmail.com>
2023-07-11 11:34:51 +0530

    new commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
     <div  className="container mx-auto flex px-0 md:px-5 py-2 md:flex-row flex-col items-center ">
       <div className="lg:flex-grow md:w-1/2 sm:p-0 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center ">
       <h1 className="md:text-[40px] sm:text-3xl text-center ">Hi, <br/><br/>  Myself <span className="text-[#0014a8]">  Sadik Khan</span>,<br/><br />I'm a </h1><br/>
-      <p className="sm:text-2xl md:text-[20px] p-0 mb-8 leading-relaxed   ">{data.hero.desc}</p>
+      <p className="sm:text-2xl md:text-[20px] p-0 mb-8 leading-relaxed    px-10  ">{data.hero.desc}</p>
       
       </div>
       <div  className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 px-10">
