40fedd8eb33cc26c1351cba4721ed7f4599d22bb
sadikkhan123 <sadikkhalidkhan@gmail.com>
2023-07-11 10:28:57 +0530

    minor Changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,14 +5,14 @@ const Hero = () => {
   return (
     <>
     {/* this is the main hero section with download cv button for css the tailwind css framework is used  */}
-    <section className="  text-gray-600 body-font  -mt-56 sm:p-0 md:pt-16 md:mx-auto " id='hero'>
+    <section className="  text-gray-600 body-font  -mt-56 sm:p-0 md:pt-16 md:mx-auto mb-24 " id='hero'>
     <div  className="container mx-auto flex px-0 md:px-5 py-2 md:flex-row flex-col items-center ">
       <div className="lg:flex-grow md:w-1/2 sm:p-0 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center ">
       <h1 className="md:text-[40px] sm:text-3xl text-center ">Hi, <br/><br/>  Myself <span className="text-[#0014a8]">  Sadik Khan</span>,<br/><br />I'm a </h1><br/>
-      <p className="sm:text-2xl md:text-[20px] p-0 mb-8 leading-relaxed  w-2/3    ">{data.hero.desc}</p>
+      <p className="sm:text-2xl md:text-[20px] p-0 mb-8 leading-relaxed   ">{data.hero.desc}</p>
       
       </div>
-      <div  className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 ">
+      <div  className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 px-10">
         {/* these svgs are svg images used in website  */}
    
 <img src={img1} alt="Logo" />
--- src/components/Hero2.js
+++ src/components/Hero2.js
@@ -5,18 +5,18 @@ const Hero2 = () => {
   return (
     <>
     {/* about section just below the main hero section */}
-      <section className="text-gray-600 body-font mx-auto md:ml-20" id='about '>
-        <div className=" mx-auto flex px-5 md:py-20 md:flex-row flex-col-reverse items-center">
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-5 md:mb-0 px-6">
-<img src={img2} alt="Logo"/>
+      <section className="text-gray-600 body-font mx-auto mb-24" id='about '>
+        <div className="  flex md:flex-row flex-col-reverse items-center">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-5 md:mb-0 px-10">
+<img  src={img2} alt="Logo"/>
 
            </div>
-          <div className="lg:flex-grow md:w-1/2 sm:p-0 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center  ">
-          <h2 className=" md:text-[40px] sm:text-3xl text-center title-font text-3xl md:mb-4 font-medium  text-[#0014a8]">
+          <div className="lg:flex-grow md:w-1/2 sm:p-0   flex flex-col md:items-start md:text-left   ">
+          <h2 className=" md:text-[40px] sm:text-3xl px-10  text-3xl md:mb-4   text-[#0014a8]">
             
        A bit more about me !
       </h2>
-      <p className="sm:text-2xl md:text-[20px] p-0 mb-8 leading-relaxed  w-2/3     ">{data.hero2.desc}</p>
+      <p className="sm:text-2xl md:text-[20px] p-0 mb-8 leading-relaxed    px-10  ">{data.hero2.desc}</p>
      
       <div className="flex lg:flex-row md:flex-col">
      
